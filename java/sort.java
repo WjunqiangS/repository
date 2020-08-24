8cf99e2eabdc2d07f1e02569d32b3e233737e166
wangjunqiang <814220330@qq.com>
2020-08-24 17:42:46 +0800

    Java得到数组中最大值、选择排序、冒泡排序

---
+++ java/sort.java
@@ -0,0 +1,96 @@
+class Sort {
+	/**
+	 *	得到数组中最大值
+	 */
+	public static int getMax(int[] array) 
+	{
+		int max = 0, i = 0;
+	
+		max = array[0];
+		for (i = 1; i < array.length; i++) {
+			if (max < array[i])	{
+				max = array[i];
+			}
+		}
+
+		return max;
+	}
+	/**
+	 *	让每一个元素与后面的元素相比较，如果大（小）的就互换  
+	 *	换一次之后第一个元素就是最大（最小）
+	 *
+	 */
+	public static void selectSort(int[] array) 
+	{
+		int temp;
+		int i = 0, j = 0;
+		for (i = 0; i < array.length; i++) {
+			for (j = i + 1; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+	/**
+	 *	让相邻元素作比较，如果大（小）的话就互换
+	 *  换完一次之后最大（最小）的元素在尾部，
+	 *  之后的比较就没必要在和尾部的元素做比较
+	 */
+	public static void bubbleSort(int[] array) 
+	{
+		int temp;
+		int i = 0, j = 0;
+		for (i = 0; i < array.length; i++) {
+			for (j = 0; j < array.length - 1 - i; j++) {
+				if (array[j] > array[j + 1]) {
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+	/**
+	 * 二分法：设置三个变量（最大、最小、中间索引），让中间的值与目标值比较。
+	 * 如果目标值大，则移动最小索引变量（min = mid + 1）
+	 * 如果目标值小，则移动最大索引变量（max = mid - 1）
+	 * 注：传入的数组对象必须是有序数组（这个函数针对从小到大的数组）
+	 */
+	public static int binarySearch(int[] array, int target) 
+	{
+		int min = 0;
+		int max = array.length - 1;
+		int mid = 0;
+		while (max >= min) {
+			mid = (max + min) / 2;
+			if (target > array[mid]) {
+				min = mid + 1;
+			} else if (target < array[mid]) {
+				max = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) 
+	{
+		int i = 0;
+		int[] array = {12, 5, 7, 10, 15};
+		int index = -1;
+
+		selectSort(array);
+		for (i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		index = binarySearch(array, 10);
+		System.out.println("");
+		System.out.print(index);
+
+		System.out.println("");
+
+	}
+}
