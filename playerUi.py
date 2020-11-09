937dd9482c690efe5c070d49c2f4d57f28db613b
wangjunqiang <814220330@qq.com>
2020-11-09 13:39:03 +0800

    增加停止按钮

--- playerUi.py
+++ playerUi.py
@@ -34,10 +34,12 @@ class PlayerGui(QWidget):
         self.__back_btn = QPushButton(self)
         self.__play_btn = QPushButton(self)
         self.__forward_btn = QPushButton(self)
+        self.__stop_btn = QPushButton(self)
 
         self.__back_btn.setIcon(QIcon('./RES/back.png'))
         self.__play_btn.setIcon(QIcon('./RES/play.png'))
         self.__forward_btn.setIcon(QIcon('./RES/forward.png'))
+        self.__stop_btn.setIcon(QIcon('./RES/stop.png'))
 
         # 播放时间
         self.__time_label1 = QLabel("00:00", self)
@@ -50,12 +52,13 @@ class PlayerGui(QWidget):
         self.__time_slider.setStyle(QStyleFactory.create('Fusion'))
 
         # 设置播放按钮的布局
-        glayout.addWidget(self.__time_label1, 0, 0)
-        glayout.addWidget(self.__time_slider, 0, 1, 1, 9)
-        glayout.addWidget(self.__time_label2, 0, 10)
+        glayout.addWidget(self.__time_label1, 0, 0, 1, 1)
+        glayout.addWidget(self.__time_slider, 0, 1, 1, 12)
+        glayout.addWidget(self.__time_label2, 0, 13)
         glayout.addWidget(self.__back_btn, 1, 1, 1, 3)
         glayout.addWidget(self.__play_btn, 1, 4, 1, 3)
         glayout.addWidget(self.__forward_btn, 1, 7, 1, 3)
+        glayout.addWidget(self.__stop_btn, 1, 10, 1, 3)
         vlayout.addWidget(QLabel('语音播放区：'))
         vlayout.addWidget(self.__btn_open_files)
         vlayout.addWidget(self.__list_files)
